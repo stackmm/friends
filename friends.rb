6a897ee40af3d187eff44639f9c27393e0a389e1
Steven Stackhouse <stackmm@gmail.com>
2023-02-23 14:23:31 -0700

    add locations

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ arrays = ["Kevin", "Joe", "Kim", "Bill", "Amanda"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+locations = ["Hawaii", "Mexico", "Europe", "Japan", "New York"]
+
+puts locations.pop
